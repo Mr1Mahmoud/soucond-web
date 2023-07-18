3e7e8f6dbbe947d90bd5a4e00802e59de2e5bc60
Mr1Mahmoud <139649558+Mr1Mahmoud@users.noreply.github.com>
2023-07-18 22:34:34 +0300

    Add files via upload

---
+++ 007.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function logos(logo){
+    img.src = logo;;
+}
+function colors(color){
+    container.style.background = color;
+}
